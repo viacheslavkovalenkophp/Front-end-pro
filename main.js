7b5dc127e938b2489847f49de313a53bc8eafd19
Viacheslav Kovalenko <viacheslavkovalenkophp@gmail.com>
2024-09-09 15:40:31 +0200

    create new branches create new data into js

---
+++ main.js
@@ -0,0 +1,10 @@
+let age = 39;
+let name = "Viacheslav";
+let cobbler = true;
+console.log(age);
+console.log(name);
+console.log(cobbler);
+console.log(typeof(age));
+console.log(typeof(name));
+console.log(typeof(cobbler));
+console.log()
