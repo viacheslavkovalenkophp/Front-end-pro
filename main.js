13e2f2b34ef904707c092197024c2f86788767e9
Viacheslav Kovalenko <viacheslavkovalenkophp@gmail.com>
2024-09-09 15:52:08 +0200

    new

---
+++ main.js
@@ -0,0 +1,9 @@
+let age = 39;
+let name = "Viacheslav";
+let cobbler = true;
+console.log(age);
+console.log(name);
+console.log(cobbler);
+console.log(typeof(age));
+console.log(typeof(name));
+console.log(typeof(cobbler));
