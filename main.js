e2b680a6476ad360ba1ad9ef6537dae9efaa6e09
Viacheslav Kovalenko <viacheslavkovalenkophp@gmail.com>
2024-09-09 18:47:10 +0200

    create new branch homework-3.2 new code in js

---
+++ main.js
@@ -0,0 +1,4 @@
+let age = prompt ("Insert your age please.");
+let height =prompt ("Insert your height please.");
+let language = prompt ("Insert your language please.");
+console.log (`my age is -${age}, my height is -${height},my language is -${language}`);
