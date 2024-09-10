1e3b7b25f2231f9d7aec84923bb1985f782b1567
Viacheslav Kovalenko <viacheslavkovalenkophp@gmail.com>
2024-09-10 16:09:18 +0200

    changes in js

--- main.js
+++ main.js
@@ -2,12 +2,13 @@ let stringNumber = "10369";
 let realNumber =+stringNumber;
 
 let fifthNumber = realNumber % 10;
-let fourthNumber = Math.floor(realNumber / 10 -1030);
-let thirdNumber = Math.round (realNumber / 3000);
-let secondNumber = Math.trunc(realNumber / 30000);
-let firstNumber = Math.floor(realNumber /10368 % 12 );
+let fourthNumber = Math.floor(realNumber / 10 % 10);
+let thirdNumber = Math.floor (realNumber / 100 % 10);
+let secondNumber = Math.floor(realNumber / 1000 % 10);
+let firstNumber = Math.floor(realNumber / 10000 % 10 );
 
 console.log(stringNumber);
 console.log(realNumber);
 
 console.log(`${firstNumber} ${secondNumber} ${thirdNumber} ${fourthNumber} ${fifthNumber}`);
+
