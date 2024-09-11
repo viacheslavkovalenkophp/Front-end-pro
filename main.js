9a3c148b508e8e3141ffe10d9344ea01790a1d10
Viacheslav Kovalenko <viacheslavkovalenkophp@gmail.com>
2024-09-11 18:56:08 +0200

    new data into js

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-let age = 39;
-let name = "Viacheslav";
-let cobbler = true;
-console.log(age);
-console.log(name);
-console.log(cobbler);
-console.log(typeof(age));
-console.log(typeof(name));
-console.log(typeof(cobbler));
+let number = prompt ("enter some number");
+let coast = +number;
+
+let thirdNumber = coast % 10;
+let secondNumber = Math.floor (coast /10 % 10);
+let firstNumber = Math.floor (coast / 100 % 10);
+
+console.log(thirdNumber);
+console.log(secondNumber);
+console.log(firstNumber);
+
+if (firstNumber === secondNumber && secondNumber === thirdNumber) {
+    console.log("the numbers is same");
+} else {
+    console.log("the numbers not is same");
+}
+if (firstNumber === secondNumber && secondNumber--- secondNumber) {
+    console.log("yes there are is same numbers");
+} else {
+    console.log("no there are is same numbers");
+}
