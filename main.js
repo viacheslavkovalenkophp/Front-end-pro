a97f1a57bd14ef5e149fcca48a66876a012b6a5d
Viacheslav Kovalenko <viacheslavkovalenkophp@gmail.com>
2024-09-16 12:29:29 +0200

    new data into js

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-let age = 39;
-let name = "Viacheslav";
-let cobbler = true;
-console.log(age);
-console.log(name);
-console.log(cobbler);
-console.log(typeof(age));
-console.log(typeof(name));
-console.log(typeof(cobbler));
+let userNumber = +prompt ("Enter number")
 
 
 
+for(let i = 1; i <= userNumber; i++){
+    if(userNumber % 2 === 0){
+        console.log("Ok")
+    } else {
+        console.log("No")
+    }
+}
