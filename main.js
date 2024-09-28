c7bfd94b4ac79d4b605222a1a7cc6a5943cdcced
Viacheslav Kovalenko <viacheslavkovalenkophp@gmail.com>
2024-09-28 13:58:40 +0200

    new files in js

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
-let age = 39;
-let name = "Viacheslav";
-let cobbler = true;
-console.log(age);
-console.log(name);
-console.log(cobbler);
-console.log(typeof(age));
-console.log(typeof(name));
-console.log(typeof(cobbler));
+const array = [1, 3, 4, 6, 2, 5, 7];
 
+for(let i =0; i < array.length; i++) {
+    if(array[i] === 4) {
+       array.splice(i,1);
 
-
+    }
+}
+console.log(array);
