bb995ec249abe78869f6b27cf4c9ffb6e1fdf94f
Viacheslav Kovalenko <viacheslavkovalenkophp@gmail.com>
2024-10-04 19:28:19 +0200

    new data into js

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
-function multiNumbers (first) {
-    return function (second) {
-        return first * second;
+let number = + prompt ("Enter Number")
+
+function superNumber () {
+
+    for(let i = 0; i < 10 ; i ++) {
+    if (number <= 100) {
+        console.log("OK");
+    } else if (number > 100) {
+        break ;
+    } else if (isNaN(number)) {
+        console.log("Enter number one more time");
+    } else {
+        console.log("?");
     }
 }
-console.log(multiNumbers(5)(2));
+}
+
+superNumber();
