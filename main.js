bf2b800c1a12984b71e2eea9cf50fc3060469d7a
Viacheslav Kovalenko <viacheslavkovalenkophp@gmail.com>
2024-10-11 15:16:20 +0200

    new data in js

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
-let age = 39;
-let name = "Viacheslav";
-let cobbler = true;
-console.log(age);
-console.log(name);
-console.log(cobbler);
-console.log(typeof(age));
-console.log(typeof(name));
-console.log(typeof(cobbler));
+let company = {
+    sles: [{name: `John`, salary: 1000}, {name: `Alice`, salary: 600}],
+    development: {
+        web: [{name: `Peter`, salary: 2000}, {name: `Alex`, salary: 1800}],
+        internals: [{name: `Jack`, salary: 1300}]
+    }
+};
 
+function collectSalary (company) {
+    let salary = [];
 
+    if (Array.isArray(company)) {
+        company.forEach(person => {
+            salary.push(person.salary);
+        });
+    } else {
+        Object.keys(company).forEach(department => {
+            salary = salary.concat(collectSalary(company[department]));
+        });
+    }
 
+    return salary;
+}
+
+console.log(collectSalary(company));
