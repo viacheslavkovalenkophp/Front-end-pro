6346aa385b01459d763666ad7c8b639717e80a03
Viacheslav Kovalenko <viacheslavkovalenkophp@gmail.com>
2024-10-16 19:46:41 +0200

    new data intojs

--- main.js
+++ main.js
@@ -1,32 +1,5 @@
-let info = +prompt("Хотите увидеть данние о субьекте введіть код 122?");
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
-if(info === 122) {
-    let onePersonObject = {
-        name: "Henrih",
-        surName: "Goldschtain",
-        age: 39,
-        placeOfBirth: "Manhattan",
-        placeOfResidence: "Canada",
-        phoneNumber: +9878532065,
-        email: "radanienarada@gmail.com",
+let evenNumbers = numbers.filter(mun => mun % 2 === 0 );
 
-
-        showInformation :function  () {
-            console.log(`Ім'я Користувача ${this.name}`);
-            console.log(`Прізвище Користувача ${this.surName}`);
-            console.log(`Вік Користувача ${this.age}`);
-            console.log(`місце народження Користувача ${this.placeOfBirth}`);
-            console.log(`місце знаходження Користувача ${this.placeOfResidence}`);
-            console.log(`номер телефону Користувача ${this.phoneNumber}`);
-            console.log(`електронна пошта Користувача ${this.email}`);
-            alert("данние у консолі");
-
-        }
-    };
-
-    onePersonObject.showInformation();
-} else {
-    console.log("Пробачте але ми не надаємо інформацію");
-    alert("Для Вас доступ заборонено");
-
-}
+console.log(evenNumbers);
