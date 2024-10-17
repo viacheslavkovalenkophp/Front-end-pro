5b2fa07334afa0711448097b6be96731bcaa8d3c
Viacheslav Kovalenko <viacheslavkovalenkophp@gmail.com>
2024-10-17 18:41:50 +0200

    new data js

--- main.js
+++ main.js
@@ -1,5 +1,53 @@
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+let enterNumber = prompt ("Enter name witch you wanna see");
 
-let evenNumbers = numbers.filter(mun => mun % 2 === 0 );
+const contactData = {
+    contactList: [
+        {
+            contactName: "Henrih",
+            phoneNumber: +48658952317,
+            address: "Uganda",
+            email: "Uganda@.ug"
+        },
+        {
+            contactName: "Bambei",
+            phoneNumber: +35698564021,
+            address: "Cibertron",
+            email: "Cibertron@ci"
+        },
+        {
+            contactName: "Ulrisch",
+            phoneNumber: +41589256745,
+            address: "Nineland",
+            email: "Nineland@ni"
+        },
+        {
+            contactName: "Hund",
+            phoneNumber: +78956214028,
+            address: "Neverland",
+            email: "Neverland@ne"
+        },
+        {
+            contactName: "Hikamoto",
+            phoneNumber: +95632589154,
+            address: "Typan",
+            email: "Typan@ty"
+        }
+    ],
+    getContactData () {
+        let found = false;
+        for(let contact of this.contactList) {
+            if(enterNumber === contact.contactName) {
+                console.log(`Ім'я ${contact.contactName}`);
+                console.log(`Номер телефону ${contact.phoneNumber}`);
+                console.log(`Країна проживання ${contact.address}`);
+                console.log(`Едектронна пошта ${contact.email}`);
+                found = true; break;
+            }
+        }   if (!found) {
+            console.log("Немає таких данних");
+    }
+}
+}
+contactData.getContactData ();
 
-console.log(evenNumbers);
+
