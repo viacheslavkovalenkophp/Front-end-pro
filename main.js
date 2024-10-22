7608d7f03da5244e9679e251f22609c2a5d1393e
Viacheslav Kovalenko <viacheslavkovalenkophp@gmail.com>
2024-10-22 20:28:32 +0200

    new data js

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-let age = 39;
-let name = "Viacheslav";
-let cobbler = true;
-console.log(age);
-console.log(name);
-console.log(cobbler);
-console.log(typeof(age));
-console.log(typeof(name));
-console.log(typeof(cobbler));
+const myFirstTable = document.querySelector('#myFirstTable');
 
-
-
+const myTable = document.createElement('myTable');
+myTable.style.borderSpacing = '10px';
+for(let tr = 0 ; tr <= 9 ; tr ++) {
+    const row = document.createElement('tr');
+    for(let td = 0 ; td <= 9 ; td ++) {
+        const cage = document.createElement('td');
+        cage.style.backgroundColor = 'yellow'
+        cage.style.gap = '20px';
+        cage.style.border = '5px solid red';
+        cage.style.width = '40px';
+        cage.style.height = '40px';
+        row.appendChild(cage);
+    }
+    myTable.appendChild(row);
+}
+myFirstTable.appendChild(myTable);
