b4837ea070b2adac7fd704252d64b1c0623c1d5f
Viacheslav Kovalenko <viacheslavkovalenkophp@gmail.com>
2024-10-22 21:20:31 +0200

    new data in js

--- main.js
+++ main.js
@@ -1,18 +1,7 @@
-const myFirstTable = document.querySelector('#myFirstTable');
+const button = document.querySelector('#button');
+const span = document.querySelector('#mySpan');
 
-const myTable = document.createElement('myTable');
-myTable.style.borderSpacing = '10px';
-for(let tr = 0 ; tr <= 9 ; tr ++) {
-    const row = document.createElement('tr');
-    for(let td = 0 ; td <= 9 ; td ++) {
-        const cage = document.createElement('td');
-        cage.style.backgroundColor = 'yellow'
-        cage.style.gap = '20px';
-        cage.style.border = '5px solid red';
-        cage.style.width = '40px';
-        cage.style.height = '40px';
-        row.appendChild(cage);
-    }
-    myTable.appendChild(row);
-}
-myFirstTable.appendChild(myTable);
+button.addEventListener('click', function ()  {
+    span.style.color = span.style.color === 'red' ? '' : 'red';
+    console.log('Clicked');
+});
