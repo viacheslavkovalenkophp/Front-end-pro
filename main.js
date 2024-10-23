1a4b28eee9c777f0e9fbecc7eda96c18fd13c9c9
Viacheslav Kovalenko <viacheslavkovalenkophp@gmail.com>
2024-10-23 16:17:16 +0200

    new data into JS

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-const button = document.querySelector('#button');
-const span = document.querySelector('#mySpan');
+const imgToChange = document.querySelector('#imgToChange');
+const one = 1;
+const two = 10;
+myNumberRandom = Math.floor(Math.random() * (two - one + 1) + one);
 
-button.addEventListener('click', function ()  {
-    span.style.color = span.style.color === 'red' ? '' : 'red';
+const imgButton = document.querySelector('#buttonImg');
+
+imgButton.addEventListener('click', function ()  {
+    myNumberRandom = Math.floor(Math.random() * (two - one + 1) + one);
+    imgToChange.src =`./image/${myNumberRandom}.webp`
     console.log('Clicked');
 });
