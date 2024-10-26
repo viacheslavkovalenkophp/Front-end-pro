5d24754c3bf9526f256c5450d5790028d725980a
Viacheslav Kovalenko <viacheslavkovalenkophp@gmail.com>
2024-10-26 12:20:08 +0200

    new data into JS

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
-
-
-let myPrompt = "";
-const promptEnter = document.querySelector('#promptEnter');
-const goToUrl = document.querySelector('#goToUrl')
-promptEnter.addEventListener('click', () => {
-     myPrompt = prompt ('Enter URL please');
+const body = document.querySelector('#body');
+body.addEventListener('mouseup', (event) => {
+    console.log(event.target);
+    console,log
 });
-myPrompt = "";
 
-goToUrl.addEventListener('click', () => {
-    location.href = `http://${myPrompt}`;
-});
+const btn1 = document.createElement('button');
+btn1.textContent = 'button1';
+btn1.id = 'btn1';
+btn1.classList.add('red');
+body.appendChild(btn1);
+
+const btn2 = document.createElement('button');
+btn2.textContent = 'button2';
+btn2.id = 'btn2';
+body.appendChild(btn2);
+
+const btn3 = document.createElement('button');
+btn3.textContent = 'button3';
+btn3.id = 'btn3';
+body.appendChild(btn3);
