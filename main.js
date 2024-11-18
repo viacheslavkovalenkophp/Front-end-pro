c9342416304a201b9b3179f717d20b769e458143
Viacheslav Kovalenko <viacheslavkovalenkophp@gmail.com>
2024-11-18 21:47:02 +0100

    new js

--- main.js
+++ main.js
@@ -1,12 +1,34 @@
-let age = 39;
-let name = "Viacheslav";
-let cobbler = true;
-console.log(age);
-console.log(name);
-console.log(cobbler);
-console.log(typeof(age));
-console.log(typeof(name));
-console.log(typeof(cobbler));
+class Calculator {
+
+    #numberOne;
+    #numberTwo;
+
+    constructor(numberOne, numberTwo) {
+        this.#numberOne = numberOne;
+        this.#numberTwo = numberTwo;
+    }
 
 
+    add (numberOne, numberTwo) {
+        return numberOne + numberTwo;
+    }
 
+    subtract (numberOne, numberTwo) {
+        return numberOne - numberTwo;
+    }
+
+    multiply (numberOne, numberTwo) {
+        return numberOne * numberTwo;
+    }
+
+    divide (numberOne, numberTwo) {
+        return numberOne / numberTwo;
+    }
+}
+
+const calc = new Calculator();
+
+console.log (calc.add(5,3));
+console.log (calc.subtract(10,4));
+console.log (calc.multiply(3,6));
+console.log (calc.divide(8,2));
