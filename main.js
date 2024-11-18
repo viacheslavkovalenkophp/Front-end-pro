703f933336af5baac2689147f98771d433dd5a12
Viacheslav Kovalenko <viacheslavkovalenkophp@gmail.com>
2024-11-18 22:26:31 +0100

    new js

--- main.js
+++ main.js
@@ -1,34 +1,19 @@
-class Calculator {
-
-    #numberOne;
-    #numberTwo;
-
-    constructor(numberOne, numberTwo) {
-        this.#numberOne = numberOne;
-        this.#numberTwo = numberTwo;
+class Coach {
+    constructor(name,speciality,rating) {
+        this.name = name;
+        this.speciality = speciality;
+        this.rating = rating;
+    }
+    displayInfo () {
+        console.log (`Coach ${this.name} ${this.speciality} ${this.rating}`)
     }
 
-
-    add (numberOne, numberTwo) {
-        return numberOne + numberTwo;
-    }
-
-    subtract (numberOne, numberTwo) {
-        return numberOne - numberTwo;
-    }
-
-    multiply (numberOne, numberTwo) {
-        return numberOne * numberTwo;
-    }
-
-    divide (numberOne, numberTwo) {
-        return numberOne / numberTwo;
-    }
 }
 
-const calc = new Calculator();
+const coach1 = new Coach('John Doe', 'Fitness', 4.7);
 
-console.log (calc.add(5,3));
-console.log (calc.subtract(10,4));
-console.log (calc.multiply(3,6));
-console.log (calc.divide(8,2));
+const coach2 = new Coach('Alice Smith', 'Yoga', 4.9);
+
+coach1.displayInfo();
+
+coach2.displayInfo();
