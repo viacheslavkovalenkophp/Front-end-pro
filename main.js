d9622a7ae914c5c6cee4d38ceefc1f36c15dd528
Viacheslav Kovalenko <viacheslavkovalenkophp@gmail.com>
2024-11-27 17:57:06 +0100

    new data js

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
-let age = 39;
-let name = "Viacheslav";
-let cobbler = true;
-console.log(age);
-console.log(name);
-console.log(cobbler);
-console.log(typeof(age));
-console.log(typeof(name));
-console.log(typeof(cobbler));
+const apiKey = '17997f2169fa8bc3c87af47d5f493ef0';
+const city = 'Kharkiv';
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 
+const updateButton = document.querySelector('#update');
+const weatherDisplay = document.querySelector('#weather');
 
-
+function fetchWeather() {
+    fetch(url)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Ошибка запроса');
+            }
+            return response.json();
+        })
+        .then((data) => {
+            const { main, weather, name } = data;
+            weatherDisplay.innerHTML = `
+        <h2>Погода в ${name}</h2>
+        <p>${weather[0].description}</p>
+        <p>Температура: ${main.temp}°C</p>
+        <p>Влажность: ${main.humidity}%</p>
+      `;
+        })
+        .catch((error) => {
+            weatherDisplay.innerHTML = `<p>Не удалось получить данные: ${error.message}</p>`;
+        });
+}
+fetchWeather();
+updateButton.addEventListener('click', fetchWeather);
